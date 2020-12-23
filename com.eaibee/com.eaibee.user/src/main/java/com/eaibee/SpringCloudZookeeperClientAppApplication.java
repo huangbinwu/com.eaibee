b86cf86c8b419170386fd0d9748cadfaf6b8fdb2
huangbinwu <452877940@qq.com>
2020-12-23 16:23:33 +0800

    1

--- com.eaibee/com.eaibee.user/src/main/java/com/eaibee/SpringCloudZookeeperClientAppApplication.java
+++ com.eaibee/com.eaibee.user/src/main/java/com/eaibee/SpringCloudZookeeperClientAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 /**
